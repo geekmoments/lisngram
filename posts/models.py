9badee9cb8625ee895ed6290cb2e5f16152d2355
Cesar Cruz <cesaraulcg@gmail.com>
2021-01-22 13:13:32 -0500

    first migrate and first database table with models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,20 @@
+"""posts models"""
+
+#django
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.EmailField()
+    password = models.CharField()
+
+    first_name = models.CharField()
+    last_name = models.CharField()
+    birthdate = models.DateField()
+
+    bio = models.TextField()
+
+    created = models.DateTimeField()
+    modifies = models.DateTimeField()
+    
